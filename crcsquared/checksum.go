4b93e86ad67dabcd0c370c491c663329f24d95b9
morsecodist <todd@morsecodist.io>
2020-07-30 13:21:26 -0400

    stop redefining table

--- crcsquared/checksum.go
+++ crcsquared/checksum.go
@@ -10,11 +10,12 @@ import (
 	"golang.org/x/exp/mmap"
 )
 
+// This ensures that we use the crc32c system command if available
+//   I stepped though the code to verify
+var crc32q *crc32.Table = crc32.MakeTable(crc32.Castagnoli)
+
 // CRC32CChecksum computes the crc32c checksum of a file
 func CRC32CChecksum(data []byte) (uint32, error) {
-	// This ensures that we use the crc32c system command if available
-	//   I stepped though the code to verify
-	crc32q := crc32.MakeTable(crc32.Castagnoli)
 	return crc32.Checksum(data, crc32q), nil
 }
 
